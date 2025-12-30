a91b1141f9e2c9bacb1c6057fb50a6210b52ef6e
Josef Christian <110720433+Vill0701@users.noreply.github.com>
2025-12-30 14:22:45 +0700

    Update vendorNo assignment to include additional field

--- purchase_b1/script.js
+++ purchase_b1/script.js
@@ -219,7 +219,7 @@ function convertDataToXml(
       ] ??
       row["Nama Penjual BKP/BKP Tidak Berwujud/Pemberi JKP"];
 
-    const vendorNo = row["NPWP Penjual"] ?? row["NPWP"];
+    const vendorNo = row["Nomor Identitas WP"] ?? row["NPWP Penjual"] ?? row["NPWP"];
 
     const dpp = parseFloat(
       row["Harga Jual/Penggantian/DPP"] ??
